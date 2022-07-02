916148dd45624c72b166d183b65d3996b1bb35f8
emimontanari <emimontanari@icloud.com>
2022-07-02 12:57:18 -0300

    Subiendo proyecto

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,63 @@
+import {useEffect, useState} from 'react'
+import {CircularProgressbar, buildStyles} from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css';
+
+const ControlPresupuesto = ({presupuesto,gastos}) => {
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
+    const [porcentaje, setPorcentaje] = useState(0)
+
+    useEffect(()=>{
+        const totalGastado = gastos.reduce((total, gasto)=> gasto.cantidad + total, 0)
+        const totalDisponible = presupuesto - totalGastado
+        //Calcular el porcentaje gastado
+        const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2);
+
+        
+        setDisponible(totalDisponible)
+        setGastado(totalGastado)
+
+        setTimeout(() => {
+            setPorcentaje(nuevoPorcentaje)
+        }, 1200);
+
+    },[gastos])
+
+    const formatearCantidad = (cantidad) =>{
+        return cantidad.toLocaleString('en-US',{
+            style: 'currency',
+            currency:'USD'
+        })
+    }
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
+        <div>
+            <CircularProgressbar 
+            value={porcentaje} 
+            styles={buildStyles({
+                pathColor: porcentaje > 100  ? '#FF1700' : '#3B44F6',
+                trailColor:'#F9F2ED',
+                textColor:porcentaje > 100  ? '#FF1700' : '#3B44F6'
+            })}
+            text={`${porcentaje}% Gastado`}
+            />
+        </div>
+        <div className='contenido-presupuesto'>
+            <p>
+                <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
+            </p>
+
+            <p className={`${disponible < 0 ? 'negativo' : ''}`}>
+                <span>Disponible: </span>{formatearCantidad(disponible)}
+            </p>
+
+            <p>
+                <span>Gastado: </span>{formatearCantidad(gastado)}
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default ControlPresupuesto
